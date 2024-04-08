d4c06fead385c58fc8532da9f7277c08cd7ed968
Anuragchandra221 <pothaluranurag2002@gmail.com>
2024-04-08 13:30:13 +0530

    styled website

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os
 import pathlib
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if(request.method=="GET"):
+        return render_template('home.html')
+    else:
+        text_input = request.form['text']
+        processed_text = "You entered: " + text_input
+        return jsonify({'result': processed_text })
+
 
 if __name__=="__main__":
     app.run(debug=True)
