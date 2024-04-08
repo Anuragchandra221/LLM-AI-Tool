954cde2dec364a21eee5cc120ecda8db61cbe9c1
Anuragchandra221 <pothaluranurag2002@gmail.com>
2024-04-08 13:58:29 +0530

    styled home page

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def home():
     if(request.method=="GET"):
         return render_template('home.html')
     else:
-        text_input = request.form['text']
+        text_input = request.json['text']
+        print(text_input)
+        # return render_template('home.html', data=text_input)
         processed_text = "You entered: " + text_input
         return jsonify({'result': processed_text })
 
