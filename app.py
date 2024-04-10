3b97c8ea81dd3bf9f929ca9907c4cd33955782b5
Anuragchandra221 <pothaluranurag2002@gmail.com>
2024-04-10 14:58:41 +0530

    Voice integrated

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, jsonify
 import os
 import pathlib
 from flask import request
+from pydub import AudioSegment
 from sql_llm import sql_to_llm
+from werkzeug.datastructures import ImmutableMultiDict
+from moviepy.editor import VideoFileClip
+from voice_to_text import voice_to_text
 
 app = Flask(__name__)
 
@@ -11,19 +15,37 @@ def home():
     if(request.method=="GET"):
         return render_template('home.html')
     else:
+        # print(request.files)
         print(request.form)
         text_input = request.form['message']
         username = "root"
         password = "root"
         host = "localhost"
         database =  "atliq_tshirts"
-
+        flag = 0
+        if text_input!="":
+            text_input
+        elif 'audio' in request.files:
+            audio_file = request.files['audio']
+            print('audio file', audio_file)
+            audio_file.save("audio.webm")
+            audio = AudioSegment.from_file("audio.webm", fomat="webm")
+            audio.export("audio.wav", format="wav")
+            text_input = voice_to_text("audio.wav")
+            print(text_input)
+            flag = 1
+         
+      
         sql_res = sql_to_llm(username, password, host, database, text_input)
         # print("ti", text_input)
         # return render_template('home.html', data=text_input)
-        processed_text = sql_res["result"]
-        # print("pre", processed_text) 
-        return jsonify(result = processed_text )
+        if flag==1:
+            processed_text = sql_res["result"]
+            return jsonify(result = processed_text, query=sql_res["query"] )
+        else:
+            processed_text = sql_res["result"]
+            return jsonify(result = processed_text)
+            # print("pre", processed_text) 
         
     # return jsonify({'result': processed_text})
 
