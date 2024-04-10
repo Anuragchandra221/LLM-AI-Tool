a61813462b98aa49a2887698b5696a5ad51d91d0
Anuragchandra221 <pothaluranurag2002@gmail.com>
2024-04-10 11:14:43 +0530

    Integrated llm file and styled the website

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import os
 import pathlib
 from flask import request
+from sql_llm import sql_to_llm
 
 app = Flask(__name__)
 
@@ -12,12 +13,19 @@ def home():
     else:
         print(request.form)
         text_input = request.form['message']
-        print(text_input)
+        username = "root"
+        password = "root"
+        host = "localhost"
+        database =  "atliq_tshirts"
+
+        sql_res = sql_to_llm(username, password, host, database, text_input)
+        # print("ti", text_input)
         # return render_template('home.html', data=text_input)
-        processed_text = "You entered: " + text_input
-        # return jsonify(result = processed_text )
+        processed_text = sql_res["result"]
+        # print("pre", processed_text) 
+        return jsonify(result = processed_text )
         
-    return jsonify({'result': processed_text})
+    # return jsonify({'result': processed_text})
 
 
 if __name__=="__main__":
--- sql_llm.py
+++ sql_llm.py
@@ -6,15 +6,21 @@ llm = GoogleGenerativeAI(model="models/text-bison-001",google_api_key=secret_key
 
 from langchain_community.utilities import SQLDatabase
 
-username = "root"
-localhost = "localhost"
-password = "root"
-port = 3306
-database = "atliq_tshirts"
-db = SQLDatabase.from_uri(f"mysql+pymysql://{username}:{password}@{localhost}/{database}", sample_rows_in_table_info=3)
-print(db.table_info)
+def sql_to_llm(username, password, host, database, query):
+    username = username
+    localhost = host
+    password = password
+    port = 3306
+    database = database
+    db = SQLDatabase.from_uri(f"mysql+pymysql://{username}:{password}@{localhost}/{database}", sample_rows_in_table_info=3)
 
-from langchain_experimental.sql import SQLDatabaseChain
+    from langchain_experimental.sql import SQLDatabaseChain
 
-db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
-db_chain("How many tshirts are there?")
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+    result = db_chain(query)
+    return {
+        "query": result['query'], 
+        "result": result['result']
+    }
+# res = sql_to_llm("root", "root", "localhost", "atliq_tshirts", "How many tshirts are there?")
+# print(res["query"], res["result"])
